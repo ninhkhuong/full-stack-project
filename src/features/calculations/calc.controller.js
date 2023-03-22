5f252fa788e3380bdc88c8a18537c26b886cdfbe
Ninh K <ninh.bkhuong@gmail.com>
2023-03-21 22:29:03 -0400

    added error handling to cacl.controller

--- src/features/calculations/calc.controller.js
+++ src/features/calculations/calc.controller.js
@@ -1,16 +1,26 @@
 //number of elevator required commercial
 const resElv = (req, res) => {
-    const elvReq = (numFloors, numApps) => {
+    try {
+      const elvReq = (numFloors, numApps) => {
         const elevatorsRequired = Math.ceil(numApps / numFloors / 6)*Math.ceil(numFloors / 20);
         return elevatorsRequired
       }
       const numFloors = parseInt(req.query.numFloors)
       const numApps = parseInt(req.query.numApps)
+  
+      if (isNaN(numFloors) || isNaN(numApps)) {
+        throw new Error('Invalid input: numFloors and numApps must be integers.')
+      }
+  
       const numElv = elvReq(numFloors, numApps)
       res.status(200).json({ numElv })
-}
-
-module.exports = { resElv };
+    } catch (error) {
+      res.status(400).json({ error: error.message })
+    }
+  }
+  
+  module.exports = { resElv };
+  
 
 
 // const unitPrices = {
