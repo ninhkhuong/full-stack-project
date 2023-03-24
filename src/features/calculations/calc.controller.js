5b42ee26660935b3013c5e3abce1e8b583f6146b
Ninh K <ninh.bkhuong@gmail.com>
2023-03-23 23:00:09 -0400

    bug fixed calc.controller

--- src/features/calculations/calc.controller.js
+++ src/features/calculations/calc.controller.js
@@ -1,48 +1,35 @@
 //number of elevator required commercial
 const resElv = (req, res) => {
   try {
-      let elvReqres, elvReqcom;
-      const displayElvCalcResult = (buildingType) => {
-          if (buildingType == "residential"){
-              elvReqres = (numFloors, numApps) => {
-                  const elevatorsRequired = Math.ceil(numApps / numFloors / 6)*Math.ceil(numFloors / 20);
-                  return elevatorsRequired;
-              };
-          } else if (buildingType == 'commercial'){
-              elvReqcom = (numFloors, maxOccupancy) => {
-                  const elevatorsRequired = Math.ceil((maxOccupancy * numFloors) / 200)*Math.ceil(numFloors / 10);
-                  const freighElevatorsRequired = Math.ceil(numFloors / 10);
-                  return freighElevatorsRequired + elevatorsRequired;
-              };
-          } 
-      };
-
       const buildingType = req.query.buildingType;
       const numFloors = parseInt(req.query.numFloors);
       const numApps = parseInt(req.query.numApps);
       const maxOccupancy = parseInt(req.query.maxOccupancy);
       const numElevators = parseInt(req.query.numElevators);
 
-      const elvReqFn = displayElvCalcResult(buildingType);
+      const calculateElevatorsRequired = (buildingType, numFloors, numApps, maxOccupancy) => {
+          if (buildingType === "residential") {
+              return Math.ceil(numApps / numFloors / 6) * Math.ceil(numFloors / 20);
+          } else if (buildingType === "commercial") {
+              const elevatorsRequired = Math.ceil((maxOccupancy * numFloors) / 200) * Math.ceil(numFloors / 10);
+              const freighElevatorsRequired = Math.ceil(numFloors / 10);
+              return freighElevatorsRequired + elevatorsRequired;
+          } else {
+              return numElevators;
+          }
+      };
 
-      let numElvres, numElvcomm;
+      const numElv = calculateElevatorsRequired(buildingType, numFloors, numApps, maxOccupancy);
 
-      if (buildingType == "residential") {
-          numElvres = elvReqres(numFloors, numApps);
-          res.status(200).json({ numElv: numElvres });
-      } else if (buildingType == "commercial") {
-          numElvcomm = elvReqcom(numFloors, maxOccupancy);
-          res.status(200).json({ numElv: numElvcomm });
-      } else {
-          res.status(200).json({ numElv: numElevators });
-      }
+      res.status(200).json({ numElv });
 
   } catch (error) {
       res.status(400).json({ error: error.message });
   }
-}
+};
 
 module.exports = { resElv };
+
   
  
   
